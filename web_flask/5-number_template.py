e7c2644a92fbd585086eca764aae25ba6885a7ca
Youssef_Saad <6270@holbertonstudents.com>
2023-09-26 13:01:54 +0100

    Number template, fixing some bugs

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,47 +4,40 @@
 Script for a Flask web application start
 """
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """view function"""
-    return 'Hello HBNB!'
-
+    """ Function that displays "Hello HBNB!" """
+    return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """view function"""
-    return 'HBNB'
-
+def hbtnb():
+    """ Function that displays HBNB """
+    return "HBNB"
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """view function"""
-    return 'C %s' % text.replace('_', ' ')
+def hbtnb1(text):
+    """ Function that displays C followed by text """
+    return "C {}".format(text.replace("_", " "))
 
-
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text="is cool"):
-    """view function"""
-    return 'Python %s' % text.replace('_', ' ')
-
+def hbtnb2(text="is cool"):
+    """ Function that displays Python followed by text """
+    return "Python {}".format(text.replace("_", " "))
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_n(n):
-    """view function"""
-    return '%d is a number' % n
-
+def hbtnb3(n):
+    """ Function that displays n is a number only if n is a number """
+    return "{} is a number".format(n)
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template_n(n):
-    """view function"""
+def hbtnb5(n):
+    """ Function that displays an HTML page """
     return render_template('5-number.html', n=n)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=None)
