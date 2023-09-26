04a4314ca6003531b09a6bb8e7895923d0a3c933
Youssef_Saad <6270@holbertonstudents.com>
2023-09-26 13:04:57 +0100

    Number template, fixing some bugs

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -8,20 +8,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Function that displays "Hello HBNB!" """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbtnb():
     """ Function that displays HBNB """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def hbtnb1(text):
     """ Function that displays C followed by text """
     return "C {}".format(text.replace("_", " "))
+
 
 @app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
 @app.route('/python/<text>', strict_slashes=False)
@@ -29,10 +33,12 @@ def hbtnb2(text="is cool"):
     """ Function that displays Python followed by text """
     return "Python {}".format(text.replace("_", " "))
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def hbtnb3(n):
     """ Function that displays n is a number only if n is a number """
     return "{} is a number".format(n)
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def hbtnb5(n):
